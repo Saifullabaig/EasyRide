775829acf89eb9fb7fa95a2e87b7b8fef840e85f
Saif Ulla Baig S <34671@ITCINFOTECH.COM>
2020-02-29 13:24:38 +0530

    added test cases

--- easy-ride-booking-service/src/test/java/com/easyride/easyridebookingservice/EasyRideBookingServiceApplicationTests.java
+++ easy-ride-booking-service/src/test/java/com/easyride/easyridebookingservice/EasyRideBookingServiceApplicationTests.java
@@ -1,13 +1,62 @@
 package com.easyride.easyridebookingservice;
 
 //import org.junit.jupiter.api.Test;
+import com.easyride.easyridebookingservice.model.Booking;
+import com.easyride.easyridebookingservice.model.PaymentStatus;
+import com.easyride.easyridebookingservice.repo.BookRepo;
+import com.easyride.easyridebookingservice.service.BookingService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.annotation.Id;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+import java.util.ArrayList;
+import java.util.UUID;
+
+@SpringBootTest(classes = EasyRideBookingServiceApplication.class)
+@TestPropertySource("/application-test.properties")
 class EasyRideBookingServiceApplicationTests {
 
-//	@Test
-//	void contextLoads() {
-//	}
+    @Autowired
+    private BookRepo bookRepo;
 
+    @Autowired
+    private BookingService bookingService;
+
+    static Booking booking;
+
+    @BeforeAll
+    public static void BookingTest() throws Exception {
+
+//            Booking booking= new Booking();
+        booking.setBookingId(UUID.randomUUID());
+        booking.setDriverId(101);
+        booking.setUserId("saifulla");
+        booking.setSource("Bangalore");
+        booking.setDestination("Goa");
+        booking.setPickDate("02/03/2020");
+        booking.setPickTime("10:15");
+        booking.setPrice(1200.23);
+        booking.setDistance(400.34);
+        booking.setPaymentStatus(PaymentStatus.SUCCESS);
+    }
+
+    @Test
+    public void getBookingDetails() throws Exception {
+        bookingService.bookTicket(booking);
+        UUID x = booking.getBookingId();
+        try {
+            Assertions.assertEquals(1, bookingService.getBooking(x));
+            ;
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+//            booking = bookingService.getBooking(x);
+    }
 }
